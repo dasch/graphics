5788de5d0ac05d79ac35c1616e5aefba73492b6b
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-03-15 11:57:46 +0100

    Make LineRasterizer use the new LinearInterpolator

--- src/solution/line_rasterizer.h
+++ src/solution/line_rasterizer.h
@@ -30,6 +30,10 @@ namespace graphics {
     template<typename math_types>
     class MyLineRasterizer : public Rasterizer<math_types>
     {
+    private:
+
+        LinearInterpolator<math_types, typename math_types::real_type> depth_interpolator;
+
     public:
 
         typedef typename math_types::vector3_type      vector3_type;
@@ -71,9 +75,11 @@ namespace graphics {
             if (x_dominant) {
                 d = abs_2dy - (abs_2dx >> 1);
                 this->innerloop = &MyLineRasterizer::x_dominant_innerloop;
+                depth_interpolator.init(x_start, x_stop, in_vertex1[3], in_vertex2[3]);
             } else {
                 d = abs_2dx - (abs_2dy >> 1);
                 this->innerloop = &MyLineRasterizer::y_dominant_innerloop;
+                depth_interpolator.init(y_start, y_stop, in_vertex1[3], in_vertex2[3]);
             }
 
             this->Debug = false;
@@ -117,7 +123,8 @@ namespace graphics {
             if (!this->valid) {
             throw std::runtime_error("MyLineRasterizer::depth():Invalid State/Not Initialized");
             }
-            return 0.0;
+
+            return depth_interpolator.value();
         }
 
         vector3_type position() const 
@@ -157,6 +164,7 @@ namespace graphics {
             // Dereference a pointer to a private member function.
             // It looks strange, but it is the way to do it!
             (this->*innerloop)();
+            depth_interpolator.next_value();
         }
 
         void print_variables()
