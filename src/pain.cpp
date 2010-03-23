38cacb11c24f67511dde344d0862f67336f55add
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-03-23 21:39:29 +0100

    Move to parser.h

---
+++ src/pain.cpp
@@ -0,0 +1,19 @@
+
+#include "graphics/graphics.h"
+#include "solution/linear_interpolator.h"
+#include "solution/point_rasterizer.h"
+#include "solution/line_rasterizer.h"
+#include "solution/triangle_rasterizer.h"
+#include "solution/math_types.h"
+#include "solution/camera.h"
+#include "solution/vertex_program.h"
+#include "solution/fragment_program.h"
+#include "readdata.h"
+
+
+int
+main(int argc, char **argv)
+{
+
+    return 0;
+}
