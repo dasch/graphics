d9355f7b23f36458d57855d17bf98021c449b84a
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-03-23 21:49:00 +0100

    Clean up the parser

--- src/solution/parser.h
+++ src/solution/parser.h
@@ -88,101 +88,93 @@ parse_data_file(const char *filename)
               break;
           case READ_VERTICES:
             if (InputBuffer[0] != '#') {
-                if (sscanf(InputBuffer, "%d%lf%lf%lf",
-                           &VertexNumber, &x, &y, &z) != 4) {
-                    cerr << "vertex not found in data file: "
-                     << filename << endl << flush;
+                if (sscanf(InputBuffer, "%d%lf%lf%lf", &VertexNumber, &x, &y, &z) != 4) {
+                    cerr << "vertex not found in data file: " << filename << endl << flush;
                     return -1;
-                }
-                else {
+                } else {
                     // insert the vertex in a data structure
 
-                cout << "Vertex " << setw(3) << VertexNumber << ": ";
+                    cout << "Vertex " << setw(3) << VertexNumber << ": ";
 
-            cout.precision(4);
-            cout << "(x, y, z) = ("
-             << setw(6) << x << ", "
-             << setw(6) << y << ", "
-             << setw(6) << z
-             << ')' << endl;
+                    cout.precision(4);
+                    cout << "(x, y, z) = ("
+                     << setw(6) << x << ", "
+                     << setw(6) << y << ", "
+                     << setw(6) << z
+                     << ')' << endl;
 
-                    if (VertexNumber == NumberOfVertices) {
+                    if (VertexNumber == NumberOfVertices)
                         state = PATCHNAME;
-                    }
-
                 }
             }
             break;
-    case PATCHNAME:
-        if (InputBuffer[0] == '#') {
-        if (strlen(InputBuffer) > 2) {
+        case PATCHNAME:
+            if (InputBuffer[0] == '#') {
+                if (strlen(InputBuffer) > 2) {
                     // read the name of the patch
-
-            if (sscanf(InputBuffer, "# %s", PatchName) != 1) {
-            cerr << "patch name not found" << endl << flush;
-            return -1;
+                    if (sscanf(InputBuffer, "# %s", PatchName) != 1) {
+                        cerr << "patch name not found" << endl << flush;
+                        return -1;
+                    }
+                    cout << "patch name: " << PatchName << endl << flush;
+                    state = SEARCH_PATCHES;
+                }
             }
-            cout << "patch name: " << PatchName << endl << flush;
-            state = SEARCH_PATCHES;
-        }
-        }
-        break;
-    case SEARCH_PATCHES:
-    case READ_PATCHES:
-        if (InputBuffer[0] == '#') {
-        if (state == READ_PATCHES) {
-            state = PATCHNAME;
-        }
-        }
-        else {
-        state = READ_PATCHES;
-        if (sscanf(InputBuffer,
-               "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d",
-               &PatchNumber,
-               &index_11, &index_12, &index_13, &index_14,
-               &index_21, &index_22, &index_23, &index_24,
-               &index_31, &index_32, &index_33, &index_34,
-               &index_41, &index_42, &index_43, &index_44) != 17) {
-            cerr << "No patch found in data file: " << filename
-             << endl << flush;
-            return -1;
-        }
-        else {
-            // insert patch in a data structure
+            break;
+        case SEARCH_PATCHES:
+        case READ_PATCHES:
+            if (InputBuffer[0] == '#') {
+                if (state == READ_PATCHES) {
+                    state = PATCHNAME;
+                }
+            } else {
+                state = READ_PATCHES;
+                if (sscanf(InputBuffer,
+                       "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d",
+                       &PatchNumber,
+                       &index_11, &index_12, &index_13, &index_14,
+                       &index_21, &index_22, &index_23, &index_24,
+                       &index_31, &index_32, &index_33, &index_34,
+                       &index_41, &index_42, &index_43, &index_44) != 17) {
+                    cerr << "No patch found in data file: " << filename
+                     << endl << flush;
+                    return -1;
+                } else {
+                    // insert patch in a data structure
 
-            cout << "patch number: "
-             << setw(2) << PatchNumber << endl << flush;
-            cout << "patch indices: " << endl << '\t'
-             << setw(6) << index_11 << ", "
-             << setw(6) << index_12 << ", "
-             << setw(6) << index_13 << ", "
-             << setw(6) << index_14 << endl;
+                    cout << "patch number: "
+                     << setw(2) << PatchNumber << endl << flush;
+                    cout << "patch indices: " << endl << '\t'
+                     << setw(6) << index_11 << ", "
+                     << setw(6) << index_12 << ", "
+                     << setw(6) << index_13 << ", "
+                     << setw(6) << index_14 << endl;
 
-            cout << '\t'
-             << setw(6) << index_21 << ", "
-             << setw(6) << index_22 << ", "
-             << setw(6) << index_23 << ", "
-             << setw(6) << index_24 << endl;
+                    cout << '\t'
+                     << setw(6) << index_21 << ", "
+                     << setw(6) << index_22 << ", "
+                     << setw(6) << index_23 << ", "
+                     << setw(6) << index_24 << endl;
 
-            cout << '\t'
-             << setw(6) << index_31 << ", "
-             << setw(6) << index_32 << ", "
-             << setw(6) << index_33 << ", "
-             << setw(6) << index_34 << endl;
+                    cout << '\t'
+                     << setw(6) << index_31 << ", "
+                     << setw(6) << index_32 << ", "
+                     << setw(6) << index_33 << ", "
+                     << setw(6) << index_34 << endl;
 
-            cout << '\t'
-             << setw(6) << index_41 << ", "
-             << setw(6) << index_42 << ", "
-             << setw(6) << index_43 << ", "
-             << setw(6) << index_44 << endl;
+                    cout << '\t'
+                     << setw(6) << index_41 << ", "
+                     << setw(6) << index_42 << ", "
+                     << setw(6) << index_43 << ", "
+                     << setw(6) << index_44 << endl;
+                }
+            }
+            break;
         }
-        }
-        break;
-    }
     }
     if (!data_file.eof()) {
-    cerr << "Something strange happend on data file: " << filename << endl;
-    return -1;
+        cerr << "Something strange happend on data file: " << filename << endl;
+        return -1;
     }
     data_file.close();
 
