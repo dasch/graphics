d27009f9aa51c7375544c953a41f38cd2c311993
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-03-29 17:51:37 +0200

    Add cameras

--- src/pain.cpp
+++ src/pain.cpp
@@ -3,6 +3,19 @@
 
 int main(int argc, char **argv)
 {
+    vrp = vector3_type(1, 0, 1);
+    vpn = vector3_type(0, 0, 1);
+    vup = vector3_type(1, 1, 1);
+    prp = vector3_type(0, -70, 70);
+
+    lower_left = vector2_type( -2.0, -2.0);
+    upper_right = vector2_type( 2.0,  2.0);
+
+    front_plane = real_type(60.0);
+    back_plane = real_type(-10.0);
+
+    render_pipeline.state().eye_position() = vector3_type(-360, -74, -13);
+
     object_t *object;
     object = object_init();
 
--- src/patches.cpp
+++ src/patches.cpp
@@ -3,10 +3,21 @@
 
 int main(int argc, char **argv)
 {
+    vrp = vector3_type(5, 5, -1);
+    vpn = vector3_type(0, 0, -1);
+    vup = vector3_type(0, 1, 0);
+    prp = vector3_type(-50, 20, 70);
+
+    lower_left = vector2_type( -12.0, -12.0);
+    upper_right = vector2_type( 12.0,  12.0);
+
+    front_plane = real_type(60.0);
+    back_plane = real_type(-10.0);
+
     object_t *object;
     object = object_init();
 
-    parse_data_file("data/pain.data", object);
+    parse_data_file("data/patches.data", object);
 
     run(object, argc, argv);
 }
